bee4de2f18300078ee7c84e628e68a68f894ffe0
Mica <hatsune@mica.lgbt>
2025-04-26 04:32:22 +0200

    justfile: Read agenix key from keychain on macOS
    That's a lot better than putting it somewhere on disk.
    
    We could probably do something like that for Linux too. It's not that
    bad on Linux since the default location is likely an in-memory
    filesystem but it's still more convenient to use the keyring daemon
    instead of having to copy stuff to a file manually after rebooting.

--- recipes/agenix.sh
+++ recipes/agenix.sh
@@ -1,8 +1,30 @@
-AGENIX_KEY=${AGENIX_KEY:-$XDG_RUNTIME_DIR/agenix-key-nixnet}
+AGENIX_KEY=${AGENIX_KEY:-}
 
+function _agenix {
+    EDITOR="${AGENIX_EDITOR:-nano -L}" agenix --identity "$1" "${@:2}"
+}
+
+# Remove preceding `secrets/` before second argument which is commonly the secret's file path
 if [[ "$#" -ge 2 ]]; then
-    set -- "${@:1:1}" "${2/#secrets\//}" "${@:3}" # Remove preceding `secrets/` before second argument which is commonly the secret's file path
+    set -- "${@:1:1}" "${2/#secrets\//}" "${@:3}"
 fi
 
 cd secrets
-EDITOR="${AGENIX_EDITOR:-nano -L}" agenix --identity "$AGENIX_KEY" "$@"
+
+if [[ -z "$AGENIX_KEY" ]]; then
+    # No explicit keyfile is set - use a default location for the current OS
+    case "$OSTYPE" in
+        darwin*)
+            # On macOS, we read the key from the default keychain
+            _agenix <(security find-generic-password -s nixnet-agenix-admin-key -w) "$@"
+            ;;
+        *)
+            # On Linux, we read it from a location that is likely an in-memory FS
+            # Could be improved to talk to the keyring daemon instead
+            _agenix "$XDG_RUNTIME_DIR/agenix-key-nixnet" "$@"
+            ;;
+    esac
+else
+    # If we have an explicit keyfile, use that
+    _agenix "$AGENIX_KEY" "$@"
+fi
