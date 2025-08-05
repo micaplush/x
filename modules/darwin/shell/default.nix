7dc0f3c9e1340f98aed9f900ba39e8b12c05d4db
Mica <hatsune@mica.lgbt>
2025-08-05 03:42:24 +0200

    mikubook/shell: Port shortcuts from fw to mikubook

--- modules/darwin/shell/default.nix
+++ modules/darwin/shell/default.nix
@@ -9,8 +9,12 @@
   };
 
   environment.shellAliases = {
+    chx = "chmod +x";
     g = "git";
+    gl = "git log";
+    gs = "git status";
     lg = "lazygit";
+    nsh = "nix-shell --packages fish --run fish";
   };
 
   environment.shells = [ pkgs.fish ];
@@ -23,6 +27,11 @@
     '';
 
     interactiveShellInit = ''
+      function rpw
+        realpath (which $argv[1])
+      end
+      complete --command rpw --exclusive --arguments '(__fish_complete_command)'
+
       complete git --condition '__fish_git_using_command fixup reword split' --no-files --keep-order --arguments '(__fish_git_recent_commits)'
 
       tailscale completion fish | source
