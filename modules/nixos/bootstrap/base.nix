cb18128744b5142928ef9f9196c5d879761f8448
Mica <hatsune@mica.lgbt>
2025-08-18 04:28:35 +0200

    bootstrap: Fix copying of `machine-id` file
    Activation scripts run before systemd is started on boot. The previous
    implementation of copying it in an activation script didn't work because
    systemd generates `/etc/machine-id`.

--- modules/nixos/bootstrap/base.nix
+++ modules/nixos/bootstrap/base.nix
@@ -18,10 +18,20 @@ in
     environment.etc.machine-id.enable = false;
     services.tailscale.authKeyFile = "${config.x.base.filesystems.persistDirectory}/tailscale-auth-key";
 
-    system.activationScripts.persist-machine-id.text = ''
-      mkdir -p /persist/etc
-      cp -f /etc/machine-id /persist/etc/machine-id
-    '';
+    systemd.services.copy-machine-id = {
+      script = ''
+        set -eu
+        mkdir -p /persist/etc
+        cp -f /etc/machine-id /persist/etc/machine-id
+      '';
+
+      serviceConfig = {
+        Restart = "on-failure";
+        RestartSec = 5;
+      };
+
+      wantedBy = [ "multi-user.target" ];
+    };
 
     x.global.netsrv.hosts.${name} = lib.mkDefault "127.0.0.1";
   };
